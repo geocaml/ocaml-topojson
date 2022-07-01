2fcf1e07aa570c11ae9aaf88419e7e8490ab5425
Jaydev <jaydev.neuroscitech@gmail.com>
2022-07-01 14:11:33 +0530

    Adding arcs field to LineString

--- src/topojson/topojson.ml
+++ src/topojson/topojson.ml
@@ -1,1 +1,16 @@
 (* Implentation of TopoJSON Objects *)
+
+module Geometry = struct
+  module LineString = struct
+    type t = { geojson : Geojson.t; arcs : int array }
+
+    (* let v = Fun.id *)
+    let v ?arcs = { geojson = Geojson.v; arcs }
+    let index = Array.get
+
+    let arcs json =
+      if index.getType = float then
+        Error (`Msg "Index must be an integer value")
+      else Ok index
+  end
+end
--- src/topojson/topojson_intf.ml
+++ src/topojson/topojson_intf.ml
@@ -1,1 +1,8 @@
 (* Types and Signatures of various modules/ functions required by topojson.ml *)
+module type Geometry = sig
+  module LineString : sig
+    type t
+
+    val v : ?arcs:int array -> t
+  end
+end
