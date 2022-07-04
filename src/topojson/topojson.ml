09505f86d662e072181cd9481ed3fcffb88ceda9
Patrick Ferris <patrick@sirref.org>
2022-07-04 09:51:18 +0100

    Code from pair programming

--- src/topojson/topojson.ml
+++ src/topojson/topojson.ml
@@ -1,16 +1,42 @@
 (* Implentation of TopoJSON Objects *)
 
-module Geometry = struct
-  module LineString = struct
-    type t = { geojson : Geojson.t; arcs : int array }
 
-    (* let v = Fun.id *)
-    let v ?arcs = { geojson = Geojson.v; arcs }
-    let index = Array.get
+module Make (J : Geojson.Json) = struct
+  module G = Geojson.Make(J)
 
-    let arcs json =
-      if index.getType = float then
-        Error (`Msg "Index must be an integer value")
-      else Ok index
+  module Geometry = struct
+
+    module Point = G.Geometry.Point
+    module LineString = struct
+      type t = int array
+      let v arcs = arcs
+    end
+
+    type geometry =
+      | Point of Point.t
+      | LineString of LineString.t
+    
+    and t = { geometry : geometry; id : G.json option; properties : (string * G.json) list; foreign_members : (string * G.json) list; }
   end
+
+  module Topology = struct
+    type t = { objects : (string * G.json) list }
+
+    let of_json json =
+      match J.find json [ "objects" ] with
+      | Some v -> Result.map (fun objects -> { objects }) (J.to_obj v)
+      | None -> Error (`Msg "No objects field in Topology object!")
+  end
+
+  type t = Topology of Topology.t | Geometry of G.Geometry.t
+
+  let of_json json = 
+    match J.find json [ "type" ] with
+    | Some typ -> (
+      match J.to_string typ with
+        | Ok "Topology" -> Topology.of_json json
+        | Ok s -> Error (`Msg ("Expected `Topology` but got " ^ s))
+        | Error _ as e -> e
+    )
+    | None -> Error (`Msg "Could not find Topology type")
 end
--- test/topojson/test.ml
+++ test/topojson/test.ml
@@ -1,1 +1,48 @@
 (* Tests to validate implemented functions/ modules  *)
+let read_file f =
+  let ic = open_in f in
+  let rec loop acc =
+    try loop (input_line ic :: acc)
+    with End_of_file ->
+      close_in ic;
+      List.rev acc
+  in
+  let lines =
+    try loop []
+    with _ ->
+      close_in ic;
+      failwith "Something went wrong reading file"
+  in
+  String.concat "\n" lines
+
+module Ezjsonm_parser = struct
+  type t = Ezjsonm.value
+
+  let catch_err f v =
+    try Ok (f v) with Ezjsonm.Parse_error (_, s) -> Error (`Msg s)
+
+  let find = Ezjsonm.find_opt
+  let to_string t = catch_err Ezjsonm.get_string t
+  let string = Ezjsonm.string
+  let to_float t = catch_err Ezjsonm.get_float t
+  let float = Ezjsonm.float
+  let to_int t = catch_err Ezjsonm.get_int t
+  let int = Ezjsonm.int
+  let to_list f t = catch_err (Ezjsonm.get_list f) t
+  let list f t = Ezjsonm.list f t
+  let to_array f t = Result.map Array.of_list @@ to_list f t
+  let array f t = list f (Array.to_list t)
+  let to_obj t = catch_err Ezjsonm.get_dict t
+  let obj = Ezjsonm.dict
+  let null = `Null
+  let is_null = function `Null -> true | _ -> false
+end
+
+module Topojson = Topojson.Make (Ezjsonm_parser)
+
+let () =
+  let json = read_file "./test/topojson/test_cases/files/exemplar.json" in
+  match Topojson.of_json (Ezjsonm.value_from_string json) with
+    | Ok { Topojson.Topology.objects } ->
+      List.iter (fun (k, _v) -> print_endline k) objects
+    | Error (`Msg m) -> failwith m
