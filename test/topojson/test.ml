2e1d1548057b6dcef720c963af4c80dfffce0127
Priceless-P <prisca.priceless11@gmail.com>
2022-11-02 00:11:56 +0100

    no errors, but test still not passing

--- test/topojson/test.ml
+++ test/topojson/test.ml
@@ -55,24 +55,12 @@ let expected_arcs =
       |];
     |]
 
-let expected_foreign_members =
-  let open Topojson in
-  Geometry.foreign_members
-
 let pp_position ppf t =
   let open Topojson.Geometry in
   let lat = Position.lat t in
   let lng = Position.lng t in
   Fmt.pf ppf "[%f, %f]" lat lng
 
-let get_foreign_members_in_point (f : Topojson.Topology.t) =
-  let open Topojson in
-  let objs = List.hd f.objects |> snd in
-  match Geometry.geometry objs with
-  | Geometry.Collection (point :: _) -> Geometry.foreign_members point
-  | _ -> assert false
-
-let s = read_file "./test_cases/files/exemplar.json"
 let position = Alcotest.testable pp_position Stdlib.( = )
 let msg = Alcotest.testable (fun ppf (`Msg m) -> Fmt.pf ppf "%s" m) Stdlib.( = )
 
@@ -109,28 +97,32 @@ let ezjsonm =
     (fun ppf t -> Fmt.pf ppf "%s" (Ezjsonm.value_to_string t))
     ezjsonm_equal
 
-(* type pp_set_formatter_out_functions = {
-     foreign_member : Topojson.Geometry.foreign_members -> unit;
+let get_foreign_members_in_point (f : Topojson.Topology.t) =
+  let open Topojson in
+  let objs = List.hd f.objects |> snd in
+  match Geometry.geometry objs with
+  | Geometry.Collection (point :: _) -> Geometry.foreign_members point
+  | _ -> assert false
 
-   } *)
+let pp_ezjsonm ppf json = Fmt.pf ppf "%s" (Ezjsonm.value_to_string json)
+
+let pp_foreign_member ppf (v : (string * Ezjsonm.value) list) =
+  Fmt.pf ppf "%a" Fmt.(list (pair string pp_ezjsonm)) v
+
+let expected_foreign_members = [ ("arcs", `A [ `Float 0. ]) ]
+let foreign_members = Alcotest.testable pp_foreign_member Stdlib.( = )
+
 let main () =
   let s = read_file "./test_cases/files/exemplar.json" in
   let json = Ezjsonm.value_from_string s in
-
-  let pp_ezjsonm ppf json = Fmt.pf ppf "%s" (Ezjsonm.value_to_string json) in
-  let pp_foreign_member ppf (v : (string * get_foreign_members_in_point) list) =
-    Fmt.pf ppf "%a" Fmt.(list (pair string pp_ezjsonm)) v
-  in
-  let foreign_members = Alcotest.testable pp_foreign_member Stdlib.( = ) in
-
   let topojson_obj = Topojson.of_json json in
   match (topojson_obj, Result.map Topojson.topojson topojson_obj) with
   | Ok t, Ok (Topojson.Topology f) ->
       (* Here we check that the arcs defined in the file are the same as the ones
          we hardcoded above *)
-      Alcotest.(check (array (array position))) "same arcs" f.arcs expected_arcs;
       Alcotest.(check foreign_members)
-        d "same foreign_member" expected_foreign_members f.foreign_members;
+        "same foreign_members" expected_foreign_members f.foreign_members;
+      Alcotest.(check (array (array position))) "same arcs" f.arcs expected_arcs;
       (* Then we check that converting the Topojson OCaml value to JSON and then back
          again produces the same Topojson OCaml value. *)
       let output_json = Topojson.to_json t in
