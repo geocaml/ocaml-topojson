3c5454e8e8796100ae0e30874ceaf0eff19d15e3
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-01-20 19:35:46 +0100

    Implement a non-blocking, streaming codec for TopoJSON  (#39)

--- src/topojsonm/topojsonm.mli
+++ src/topojsonm/topojsonm.mli
@@ -1,1 +1,50 @@
 (* Topojsonm Interface *)
+(* Copyright (c) 2021-2022 Patrick Ferris <patrick@sirref.org>
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*)
+
+(** A library for manipulating large TopoJson documents without reading the
+    whole document into memory using the {!Jsonm} streaming, JSON parser. *)
+
+module Err : sig
+  type location = (int * int) * (int * int)
+  type t = [ `Error of location * Jsonm.error | `EOI | `Unexpected of string ]
+
+  val pp : Format.formatter -> t -> unit
+end
+
+module Topo : Topojson.S with type json = Ezjsonm.value
+
+(** {2 Maps}
+
+    Maps are functions that allow you to manipulate common structure in TopoJson
+    objects. These will be written directly back to the destination that you
+    provide. *)
+
+val map_object :
+  (string * Topo.Geometry.t -> string * Topo.Geometry.t) ->
+  Jsonm.src ->
+  Jsonm.dst ->
+  (unit, Err.t) result
+(** [map_object f src dst] will apply [f] to all TopoJson objects. This is
+    essentially any
+    {{:https://datatracker.ietf.org/doc/html/rfc7946#section-3.1} geometry
+    object}.
+
+    The map will recurse into geometry collections. Note for the moment if you
+    have a single geometry object as your document, this will not work. *)
+
+module Ezjsonm = Ezjsonm
+module Jsonm = Jsonm
+module Uutf = Uutf
